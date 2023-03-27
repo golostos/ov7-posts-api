fed78886e17f737f92a85785aa925904c744c17d
Alex Dmit <alex.golostos@gmail.com>
2023-03-27 22:02:45 +0300

    Zod started

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+// @ts-check
+const { PrismaClientKnownRequestError } = require('@prisma/client/runtime')
 const express = require('express')
+const { z, ZodError } = require('zod')
 const db = require('./prisma/db')
 const app = express()
 
+// body parser (json)
 app.use(express.json())
 
 // API - application programming interface
@@ -19,17 +23,90 @@ app.get('/hello', (req, res) => {
 
 // Read
 app.get('/api/posts', async (req, res) => {
-    const allPosts = await db.post.findMany()
-    res.send(allPosts)
+    const { skip, limit } = req.query
+    if (typeof skip === 'string' && typeof limit === 'string') {
+        const posts = await db.post.findMany({
+            skip: +skip,
+            take: +limit
+        })
+        res.send(posts)
+    } else {
+        const allPosts = await db.post.findMany({})
+        res.send(allPosts)
+    }
+})
+
+// GET /api/posts/12345
+app.get('/api/posts/:postId', async (req, res) => {
+    const schema = z.number()
+    const postId = await schema.safeParseAsync(+req.params.postId)
+    if (postId.success) {
+        const post = await db.post.findUnique({
+            where: {
+                id: postId.data
+            }
+        })
+        res.send(post)
+    } else {
+        res.status(400).send({ message: 'Wrong PostID' })
+    }
 })
 
 // Create
 app.post('/api/posts', async (req, res) => {
-    const newPost = req.body
-    const newPostDB = await db.post.create({
-        data: newPost
+    const schema = z.object({
+        title: z.string(),
+        text: z.string(),
+        author: z.string(),
     })
-    res.send(newPostDB)
+    try {
+        const newPost = await schema.parseAsync(req.body)
+        const newPostDB = await db.post.create({
+            data: newPost
+        })
+        res.send(newPostDB)
+    } catch (error) {
+        if (error instanceof ZodError) {
+            res.status(400).send({
+                message: error.message
+            })
+        }
+        if (error instanceof PrismaClientKnownRequestError) {
+            res.status(400).send({
+                message: error.message,
+                code: error.code
+            })
+        }
+    }
+})
+
+// Update
+app.patch('/api/posts/:postId', async (req, res) => {
+    const schema = z.object({
+        title: z.string().optional(),
+        text: z.string().optional(),
+        author: z.string().optional(),
+    })
+    const postId = req.params.postId
+    const updatedPost = req.body
+    const updatedPostDb = await db.post.update({
+        where: {
+            id: +postId
+        },
+        data: updatedPost
+    })
+    res.send(updatedPostDb)
+})
+
+// Delete
+app.delete('/api/posts/:postId', async (req, res) => {
+    const postId = req.params.postId
+    const removedPost = await db.post.delete({
+        where: {
+            id: +postId
+        }
+    })
+    res.send(removedPost)
 })
 
 app.listen(3333)
